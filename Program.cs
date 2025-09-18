f86fdd439e5993c2a596e5e29fb2b508ac07dbb5
ousmanbittaye <ousman.j@hotmail.com>
2025-09-18 20:58:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UML_Class_Diagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
